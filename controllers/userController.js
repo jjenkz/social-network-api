8c0971c8de1acf3ba3688eb356bfedc4df0e9a24
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-07-28 23:37:59 -0700

    added js files to controllers

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+
+module.exports = {
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // get a single user by id
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user found with that ID" });
+      }
+
+      re.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // create a user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
